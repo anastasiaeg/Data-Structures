214691a1a584a48bef8233ae998faf3a000588ce
aegorov <aegorov@ncsu.edu>
2018-05-03 17:45:27 -0400

    Test binary tree

---
+++ DataStructures/src/tree/TreeTest.java
@@ -0,0 +1,66 @@
+package tree;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TreeTest {
+	
+	private BinarySearchTree<Integer> bst;
+	
+	/**
+	 * Set up before every test
+	 * @throws Exception when can't set up
+	 */
+	@Before
+	public void setUp() throws Exception {
+		bst = new BinarySearchTree<Integer>();
+	}
+	
+	@Test
+	public void testBinarySearchTreeInsert() {
+		assertEquals(0, bst.getNodes());
+		try {
+			bst.insert(null);
+			fail();
+		} catch(IllegalArgumentException e) {
+			assertEquals(0, bst.getNodes());
+		}
+		try {
+			bst.insert(8);
+			bst.insert(3);
+			bst.insert(10);
+			bst.insert(14);
+			bst.insert(6);
+			bst.insert(1);
+			bst.insert(4);
+			bst.insert(13);
+			assertEquals(8, bst.getNodes());
+			bst.insert(4);
+			bst.insert(7);
+			assertEquals(9, bst.getNodes());
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testBinarySearchTreeRemove() {
+		bst.insert(50);
+		bst.insert(30);
+		bst.insert(70);
+		bst.insert(20);
+		bst.insert(40);
+		bst.insert(60);
+		bst.insert(80);
+		assertEquals("50 30 70 20 40 60 80 ", bst.levelOrderIterative());
+		bst.remove(20);
+		assertEquals("50 30 70 40 60 80 ", bst.levelOrderIterative());
+		bst.remove(30);
+		assertEquals("50 40 70 60 80 ", bst.levelOrderIterative());
+		bst.remove(50);
+		assertEquals("60 40 70 80 ", bst.levelOrderIterative());
+	}
+}
